77ed6c6a9a707569bad8d994ac8adafd4b175abf
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-08-25 21:02:02 +0900

    feat: 헤더 사이즈설정  및 로고 추가

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import HeaderLogoSrc from '../../assets/images/header_logo.png'
+
+const Header_container =styled.header`
+  box-sizing: border-box;
+  width: 100%;
+  height: 50px;
+  background-color: yellow;
+  padding: 10px;
+`
+
+const Logo = styled.img`
+    height: 100%;
+`;
+
+// export const GlobalStyle = createGlobalStyle`
+//   body{padding:0; margin:0}
+// `;
 
 const Header = () => {
-  return <div>Header</div>;
+  return <Header_container>
+    <Logo src={HeaderLogoSrc} />
+    </Header_container>;
 };
 
 export default Header;
