d81ab28a96b2e4121d5f5357a1955907c055849a
x2d7751347m <x2d7751347m@gmail.com>
2022-08-25 14:04:15 +0900

    initial

---
+++ DeployServer/src/main/java/com/team10/preproject/PreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team10.preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectApplication.class, args);
+	}
+
+}
