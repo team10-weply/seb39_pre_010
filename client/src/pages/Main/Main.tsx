715da666ad40a33a8fabdb8926a54a73593f2270
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-08-26 23:48:23 +0900

    docs: top questions 제목 추가

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,7 +1,51 @@
+import Nav from 'components/Sidebar/Leftsidebar';
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const Main_container = styled.div`
+  display: flex;
+`;
+const TopQuestion = styled.div`
+  padding: 24px 16px;
+  height: 100vh;
+  width: 100%;
+  overflow-y: scroll;
+  h1 {
+    font-size: 2.08rem;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+    padding: 10px;
+    display: inline-block;
+    border: 1px solid black;
+    border-radius: 3px;
+  }
+`;
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Main_container>
+      <Nav />
+      <TopQuestion>
+        <h1>Top Questions</h1>
+        <div>
+          <a href="#">Ask Question</a>
+        </div>
+        <div>
+          <div>
+            <a href="#">Interesting</a>
+            <a href="#">
+              <span>307</span>Bountied
+            </a>
+            <a href="#">Hot</a>
+            <a href="#">Week</a>
+            <a href="#">Month</a>
+          </div>
+        </div>
+      </TopQuestion>
+    </Main_container>
+  );
 };
 
 export default Main;
