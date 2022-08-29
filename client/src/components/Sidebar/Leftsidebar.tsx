e1b770318ce7c3530cc084036ddc411d10167936
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-08-29 14:20:31 +0900

    docs: 사이드바 내용을 이미지로 삽입

--- client/src/components/Sidebar/Leftsidebar.tsx
+++ client/src/components/Sidebar/Leftsidebar.tsx
@@ -1,49 +1,80 @@
 import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
+import LeftbarAdSrc from '../../assets/images/leftbar_ad.png';
 
 const Left_container = styled.div`
-  box-sizing: border-box;
-  width: 164px;
   height: 100vh;
   background-color: green;
-  padding: 10px;
+  color: #b4b0af;
+  width: 164px;
+`;
+
+const Sticky_container = styled.div`
+  position: sticky;
+  width: auto;
+  overflow-y: auto;
+`;
+
+const Nav = styled.nav`
+  li {
+    margin: 20px 0 20px 10px;
+    font-size: 0.8rem;
+  }
+  li.title {
+    font-size: 0.73rem;
+  }
+  a {
+    color: #2c2c2c;
+  }
+  a:hover {
+    color: #0e0e0e;
+  }
+`;
+
+const TeamAd = styled.img`
+  display: inline-block;
+  width: 164px;
 `;
 
 const Left = () => {
   return (
     <Left_container>
-      <nav>
-        <ol>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <ol>
-              <li>PUBLIC</li>
-              <li>
-                <a href="#">Questions</a>
-              </li>
-              <li>
-                <a href="#">Tags</a>
-              </li>
-              <li>
-                <a href="#">Users</a>
-              </li>
-              <li>
-                <a href="#">Companies</a>
-              </li>
-              <li>COLLECTIVES</li>
-              <li>
-                <a href="#">Explore Collectives</a>
-              </li>
-            </ol>
-          </li>
-          <li>
-            <span>TEAMS</span>
-            <span>X</span>
-          </li>
-        </ol>
-      </nav>
+      <Sticky_container>
+        <Nav>
+          <ol>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li className="title">
+              PUBLIC
+              <ol>
+                <li>
+                  <a href="#">Questions</a>
+                </li>
+                <li>
+                  <a href="#">Tags</a>
+                </li>
+                <li>
+                  <a href="#">Users</a>
+                </li>
+                <li>
+                  <a href="#">Companies</a>
+                </li>
+              </ol>
+            </li>
+            <li className="title">
+              COLLECTIVES
+              <ol>
+                <li>
+                  <a href="#">Explore Collectives</a>
+                </li>
+              </ol>
+            </li>
+            <li className="title">TEAMS</li>
+          </ol>
+        </Nav>
+        <TeamAd src={LeftbarAdSrc} />
+      </Sticky_container>
     </Left_container>
   );
 };
