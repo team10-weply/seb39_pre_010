fe84953dd42252a99763bb89ca9a374a82258887
seonnn <ssni0105@gmail.com>
2022-08-31 01:00:13 +0900

    feat: stack overflow logo 및 social login logo 이미지 추가

---
+++ client/src/assets/images/SocialLogo/index.ts
@@ -0,0 +1,9 @@
+import googleLogo from './Google.svg';
+import githubLogo from './GitHub.svg';
+import facebookLogo from './Facebook.svg';
+
+export const SocialLogos = {
+  googleLogo,
+  githubLogo,
+  facebookLogo,
+};
