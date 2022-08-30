0e68f4dc074adef6709014419212674522affb78
x2d7751347m <x2d7751347m@gmail.com>
2022-08-30 20:21:58 +0900

    feat : modify corsmapping

--- DeployServer/src/main/java/com/team10/preproject/config/SecurityConfig.java
+++ DeployServer/src/main/java/com/team10/preproject/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
 //                .and()
 //                .logout()
 //                .permitAll();
-        http.csrf().disable();
+//        http.csrf().disable();
+        http.cors();
         http.headers().frameOptions().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
