4b7ef31d87618e82f9a808b390bcc3bc329f7617
Min Jiwon <62936128+x2d7751347m@users.noreply.github.com>
2022-08-31 13:24:48 +0900

    Update WebConfig.java

--- DeployServer/src/main/java/com/team10/preproject/config/WebConfig.java
+++ DeployServer/src/main/java/com/team10/preproject/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:8080")
-            .allowedOrigins("http://localhost:8080", "http://{aws주소}:8080",
+            .allowedOrigins("http://localhost:8080",
                         "http://localhost:3000",
                         "https://localhost:3000",
                         "https://127.0.0.1:3000"
