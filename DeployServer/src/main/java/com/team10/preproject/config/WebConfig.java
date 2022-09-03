3bc302ab77221622b1b496dfc35b2d89b8c66f29
x2d7751347m <x2d7751347m@gmail.com>
2022-09-03 19:25:49 +0900

    Fix : Minor optimizations

--- DeployServer/src/main/java/com/team10/preproject/config/WebConfig.java
+++ DeployServer/src/main/java/com/team10/preproject/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+//    @Value("${config.domain}")
+//    private String domain;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
