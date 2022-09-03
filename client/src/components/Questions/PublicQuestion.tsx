874c9803ef2fe63286c42149d89b03b15ad82a4a
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-04 00:06:21 +0900

    feat: post기능 작성 중

--- client/src/components/Questions/PublicQuestion.tsx
+++ client/src/components/Questions/PublicQuestion.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import BasicBtn from 'components/Button/BasicBtn';
+import InputBorder from 'components/Input/InputBorder';
+import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import styled from 'styled-components';
+import { IListsInfo } from './QuestionList';
+
+const QuestionForm = styled.form``;
 
 const QuestionContainer = styled.div`
   box-sizing: border-box;
   background-color: white;
   border-radius: 3px;
   padding: 16px;
+  margin-bottom: 2rem;
   line-height: 1.2rem;
   box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.06), 0 2px 6px hsla(0, 0%, 0%, 0.06),
     0 3px 8px hsla(0, 0%, 0%, 0.09);
@@ -49,40 +57,64 @@ const Body = styled.div`
 const Tags = styled.div``;
 
 const PublicQuestion = () => {
+  const [posts, setPosts] = useState<IListsInfo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const onSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!isLoading && titleRef.current && contentRef.current) {
+      setIsLoading(true);
+      const title = titleRef.current.value;
+      const content = contentRef.current.value;
+      const response = await axios
+        .post('/questions', { title: title, content: content })
+        .then((res) => {
+          setIsLoading(false);
+          setPosts([res.data, ...posts]);
+          alert('글 작성완료!');
+        })
+        .catch((err) => alert('글 작성에 실패하였습니다.'));
+    }
+  };
+
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+  const tagRef = useRef<HTMLInputElement>(null);
+
   return (
-    <QuestionContainer>
-      <Title>
-        <h2>Title</h2>
-        <p>
-          Be specific and imagine you’re asking a question to another person
-        </p>
-        <form>
-          <input
+    <QuestionForm onSubmit={onSubmit}>
+      <QuestionContainer>
+        <Title>
+          <h2>Title</h2>
+          <p>
+            Be specific and imagine you’re asking a question to another person
+          </p>
+          <InputBorder
             type="text"
             placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
-          ></input>
-        </form>
-      </Title>
-      <Body>
-        <h2>Body</h2>
-        <p>
-          Include all the information someone would need to answer your question
-        </p>
-        <form>
-          <textarea cols={92} rows={15}></textarea>
-        </form>
-      </Body>
-      <Tags>
-        <h2>Tags</h2>
-        <p>Add up to 5 tags to describe what your question is about</p>
-        <form>
-          <input
+            ref={titleRef}
+          />
+        </Title>
+        <Body>
+          <h2>Body</h2>
+          <p>
+            Include all the information someone would need to answer your
+            question
+          </p>
+          <textarea cols={92} rows={15} ref={contentRef} />
+        </Body>
+        <Tags>
+          <h2>Tags</h2>
+          <p>Add up to 5 tags to describe what your question is about</p>
+
+          <InputBorder
             type="text"
             placeholder="e.g. (angular sql-server string)"
-          ></input>
-        </form>
-      </Tags>
-    </QuestionContainer>
+            ref={tagRef}
+          />
+        </Tags>
+      </QuestionContainer>
+      <BasicBtn>Post your question</BasicBtn>
+    </QuestionForm>
   );
 };
 
