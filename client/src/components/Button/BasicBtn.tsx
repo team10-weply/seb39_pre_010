1fe51da0342a6599db109e5ea2554f2031216f82
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-05 14:12:01 +0900

    style: 필요없는 주석 삭제

--- client/src/components/Button/BasicBtn.tsx
+++ client/src/components/Button/BasicBtn.tsx
@@ -13,17 +13,4 @@ const BasicBtn = styled.button`
   }
 `;
 
-// a{
-//     text-decoration:none;
-//     color:inherit;
-// }
-// input{
-//     border: 1px solid rgb(186,191,196);
-//     :focus {
-//         outline: none !important;
-//         border-color: #94caf3;
-//         box-shadow: 0 0 0px 4px #d6edfc;
-//     }
-// }
-
 export default BasicBtn;
