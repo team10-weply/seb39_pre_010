532f64f212e2a0ca190c51e53b3f4fd33c428799
seonnn <ssni0105@gmail.com>
2022-09-14 02:03:42 +0900

    refactor: 서버 url 변경에 따른 baseURL 변경

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestHeaders } from 'axios';
 import { cookie } from 'utils/cookie';
 
 export const client = axios.create({
-  baseURL: 'http://ec2-43-201-25-81.ap-northeast-2.compute.amazonaws.com:8080',
+  baseURL: 'http://ec2-52-79-234-12.ap-northeast-2.compute.amazonaws.com:8080',
 });
 
 export const headers: AxiosRequestHeaders = {
