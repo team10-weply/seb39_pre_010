760d65b40e60df9ecb91b98947832b44324a3074
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2022-09-14 07:49:27 +0900

    refactor: 서버 url 변경에 따른 baseURL 변경

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestHeaders } from 'axios';
 import { cookie } from 'utils/cookie';
 
 export const client = axios.create({
-  baseURL: 'http://ec2-52-79-234-12.ap-northeast-2.compute.amazonaws.com:8080',
+  baseURL: 'http://ec2-52-79-233-179.ap-northeast-2.compute.amazonaws.com:8080',
 });
 
 export const headers: AxiosRequestHeaders = {
