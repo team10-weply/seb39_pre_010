33dc04ed31e7d950979af864344c9b1a9b180ad0
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2022-09-19 00:17:55 +0900

    Update index.ts

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestHeaders } from 'axios';
 import { cookie } from 'utils/cookie';
 
 export const client = axios.create({
-  baseURL: 'http://ec2-52-79-233-179.ap-northeast-2.compute.amazonaws.com:8080',
+  baseURL: '${{ secrets.EC2_Elastic_IP }}',
 });
 
 export const headers: AxiosRequestHeaders = {
